5f45389f264e88fb4b0ba5e6a30ea4b9ecc2b609
shivkoirala <shiv_koirala@yahoo.com>
2020-12-24 13:50:20 +0530

    20 lessons final commit

---
+++ HTMLandCSS/HTMLandCSS/wwwroot/Html/MyScript.js
@@ -0,0 +1,4 @@
+﻿function Callme() {
+    var name = document.getElementById("id123").value;
+    alert("You typed your name as " + name);
+}
---
+++ HTMLandCSS/HTMLandCSS/wwwroot/Html/worker.js
@@ -0,0 +1,5 @@
+﻿i = 0;
+while (i < 20000000) {
+    postMessage(i);
+    i++;
+}
